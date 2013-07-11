bab4d13c20c6ca407d200e7cb939283bd3550272
alexander rusanov <alexander.rusanov@gmail.com>
2013-07-12 00:49:26 +0400

    added on|off custom text class 'switch-animate' added on when toggle clicked

--- angular-toggle-switch.js
+++ angular-toggle-switch.js
@@ -1,18 +1,21 @@
-angular.module('toggle-switch', ['ng']).directive('toggleSwitch', function() {
-  return {
-    restrict: 'EA',
-    replace: true,
-    scope: {
-      model: '='
-    },
-    template: '<div class="switch" ng-click="toggle()"><div class="switch-animate" ng-class="{\'switch-off\': !model, \'switch-on\': model}"><span class="switch-left">On</span><span class="knob">&nbsp;</span><span class="switch-right">Off</span></div></div>',
-    link: function($scope, element, attrs) {
-      if ($scope.model == null) {
-        $scope.model = false;
-      }
-      return $scope.toggle = function() {
-        return $scope.model = !$scope.model;
-      };
-    }
-  };
+angular.module('toggle-switch', ['ng']).directive('toggleSwitch', function () {
+    return {
+        restrict: 'EA',
+        replace: true,
+        scope: {
+            model: '=',
+            on: '=',
+            off: '='
+        },
+        template: '<div class="switch" ng-click="toggle()"><div ng-class="{\'switch-off\': !model, \'switch-on\': model}"><span class="switch-left">{{ on || "On" }}</span><span class="knob">&nbsp;</span><span class="switch-right">{{ off || "Off" }}</span></div></div>',
+        link: function ($scope, element, attrs) {
+            if ($scope.model == null) {
+                $scope.model = false;
+            }
+            return $scope.toggle = function () {
+                element.children().addClass('switch-animate')
+                return $scope.model = !$scope.model;
+            };
+        }
+    };
 });
