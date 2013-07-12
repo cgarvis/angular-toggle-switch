a52160eb43cd9ea16beb86ac60c09e68e15c2dee
alexander rusanov <alexander.rusanov@gmail.com>
2013-07-12 11:25:28 +0400

    Style fixes Scope vars renamed to onLabel and offLabel

--- angular-toggle-switch.js
+++ angular-toggle-switch.js
@@ -1,21 +1,23 @@
 angular.module('toggle-switch', ['ng']).directive('toggleSwitch', function () {
-    return {
-        restrict: 'EA',
-        replace: true,
-        scope: {
-            model: '=',
-            on: '=',
-            off: '='
-        },
-        template: '<div class="switch" ng-click="toggle()"><div ng-class="{\'switch-off\': !model, \'switch-on\': model}"><span class="switch-left">{{ on || "On" }}</span><span class="knob">&nbsp;</span><span class="switch-right">{{ off || "Off" }}</span></div></div>',
-        link: function ($scope, element, attrs) {
-            if ($scope.model == null) {
-                $scope.model = false;
-            }
-            return $scope.toggle = function () {
-                element.children().addClass('switch-animate')
-                return $scope.model = !$scope.model;
-            };
-        }
-    };
+  return {
+    restrict: 'EA',
+    replace: true,
+    scope: {
+      model: '=',
+      onLabel: '=on-label',
+      offLabel: '=off-label'
+    },
+    template: '<div class="switch" ng-click="toggle()"><div ng-class="{\'switch-off\': !model, \'switch-on\': model}"><span class="switch-left">{{ onLabel }}</span><span class="knob">&nbsp;</span><span class="switch-right">{{ offLabel }}</span></div></div>',
+    link: function ($scope, element, attrs) {
+      if ($scope.model == null) {
+        $scope.model = false;
+      }
+      $scope.onLabel = $scope.onLabel || 'On'
+      $scope.offLabel = $scope.offLabel || 'Off'
+      return $scope.toggle = function () {
+        element.children().addClass('switch-animate')
+        return $scope.model = !$scope.model;
+      };
+    }
+  };
 });
