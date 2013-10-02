199f79bc492c48ef97d6e2e56df9202b96ca7585
nathan <nathan@vertile.com>
2013-10-02 16:35:55 -0700

    don't set any values on the model
    readers shouldn't update data. doing so creates side effects, esp. on models nested within models that are expected to be null

--- angular-toggle-switch.js
+++ angular-toggle-switch.js
@@ -7,9 +7,6 @@ angular.module('toggle-switch', ['ng']).directive('toggleSwitch', function() {
     },
     template: '<div class="switch" ng-click="toggle()"><div class="switch-animate" ng-class="{\'switch-off\': !model, \'switch-on\': model}"><span class="switch-left">On</span><span class="knob">&nbsp;</span><span class="switch-right">Off</span></div></div>',
     link: function($scope, element, attrs) {
-      if ($scope.model == null) {
-        $scope.model = false;
-      }
       return $scope.toggle = function() {
         return $scope.model = !$scope.model;
       };
