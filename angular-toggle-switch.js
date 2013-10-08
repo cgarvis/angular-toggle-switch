3f3b8fe4e079338a09f5ada15b7fce08a6d6742c
Christopher Garvis <cgarvis@gmail.com>
2013-10-08 13:47:57 -0400

    Do not set model to false by default
    See #5 for more information

--- angular-toggle-switch.js
+++ angular-toggle-switch.js
@@ -9,9 +9,6 @@ angular.module('toggle-switch', ['ng']).directive('toggleSwitch', function () {
     },
     template: '<div class="switch" ng-click="toggle()"><div ng-class="{\'switch-off\': !model, \'switch-on\': model}"><span class="switch-left">{{ onLabel }}</span><span class="knob">&nbsp;</span><span class="switch-right">{{ offLabel }}</span></div></div>',
     link: function ($scope, element, attrs) {
-      if ($scope.model == null) {
-        $scope.model = false;
-      }
       $scope.onLabel = $scope.onLabel || 'On'
       $scope.offLabel = $scope.offLabel || 'Off'
       return $scope.toggle = function () {
