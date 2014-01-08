83cd54d3a081829a62bf4f7c5e6e7ac56b129f38
Martin Maestri <mmaestri@gmail.com>
2014-01-08 15:07:40 -0300

    angular-toggle-switch.js:   Indentation set as original file from @cgarvis's repo angular-toggle-switch.css:   Set as original file - added disabled class

--- angular-toggle-switch.js
+++ angular-toggle-switch.js
@@ -1,38 +1,40 @@
 angular.module('toggle-switch', ['ng']).directive('toggleSwitch', function () {
-  return {
-    restrict: 'EA',
-    replace: true,
-    scope: {
-    model: '=',
-    disabled: '@',
-    onLabel: '@',
-    offLabel: '@',
-    knobLabel: '@'
-  },
-  template: '<div class="switch" ng-click="toggle()" ng-class="{ \'disabled\': disabled }"><div ng-class="{\'switch-off\': !model, \'switch-on\': model }"><span class="switch-left" ng-bind="onLabel">On</span><span class="knob" ng-bind="knobLabel">&nbsp;</span><span class="switch-right" ng-bind="offLabel">Off</span></div></div>',
-    link: function ($scope, element, attrs) {
-      attrs.$observe('onLabel', function(val) {
-        $scope.onLabel = angular.isDefined(val) ? val : 'On';
-      });
+    return {
+        restrict: 'EA',
+        replace: true,
+        scope: {
+            model: '=',
+            disabled: '@',
+            onLabel: '@',
+            offLabel: '@',
+            knobLabel: '@'
+        },
 
-      attrs.$observe('offLabel', function(val) {
-        $scope.offLabel = angular.isDefined(val) ? val : 'Off';
-      });
+        template: '<div class="switch" ng-click="toggle()" ng-class="{ \'disabled\': disabled }"><div ng-class="{\'switch-off\': !model, \'switch-on\': model }"><span class="switch-left" ng-bind="onLabel">On</span><span class="knob" ng-bind="knobLabel">&nbsp;</span><span class="switch-right" ng-bind="offLabel">Off</span></div></div>',
+        link: function ($scope, element, attrs) {
+            attrs.$observe('onLabel', function(val) {
+                $scope.onLabel = angular.isDefined(val) ? val : 'On';
+            });
 
-      attrs.$observe('knobLabel', function(val) {
-        $scope.knobLabel = angular.isDefined(val) ? val : '\u00A0';
-      });
+            attrs.$observe('offLabel', function(val) {
+                $scope.offLabel = angular.isDefined(val) ? val : 'Off';
+            });
 
-      attrs.$observe('disabled', function(disabled) {
-        $scope.disabled = disabled === 'true';
-      });
+            attrs.$observe('knobLabel', function(val) {
+                $scope.knobLabel = angular.isDefined(val) ? val : '\u00A0';
+            });
 
-      $scope.toggle = function toggle() {
-        if(!$scope.disabled) {
-          element.children().addClass('switch-animate');
-          $scope.model = !$scope.model;
+            attrs.$observe('disabled', function(disabled) {
+                $scope.disabled = disabled === 'true';
+            });
+
+            $scope.toggle = function toggle() {
+                if(!$scope.disabled) {
+                    element.children().addClass('switch-animate');
+                    $scope.model = !$scope.model;
+                }
+            };
         }
-      };
-    }
-  };
+    };
 });
+
