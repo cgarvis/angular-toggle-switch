48810a04f6c9d1eaa7b46e533f8a284afcace581
Martin Maestri <mmaestri@gmail.com>
2014-01-08 11:56:46 -0200

    Update angular-toggle-switch.js

--- angular-toggle-switch.js
+++ angular-toggle-switch.js
@@ -4,13 +4,12 @@ angular.module('toggle-switch', ['ng']).directive('toggleSwitch', function () {
     replace: true,
     scope: {
       model: '=',
-      enabled: '=',
-      editable: '=',
+      disabled: '@',
       onLabel: '@',
       offLabel: '@',
       knobLabel: '@'
     },
-    template: '<div class="switch" ng-click="toggle()" ng-class="{ \'disabled\': !enabled }"><div ng-class="{\'switch-off\': !model, \'switch-on\': model }"><span class="switch-left" ng-bind="onLabel">On</span><span class="knob" ng-bind="knobLabel">&nbsp;</span><span class="switch-right" ng-bind="offLabel">Off</span></div></div>',
+    template: '<div class="switch" ng-click="toggle()" ng-class="{ \'disabled\': disabled }"><div ng-class="{\'switch-off\': !model, \'switch-on\': model }"><span class="switch-left" ng-bind="onLabel">On</span><span class="knob" ng-bind="knobLabel">&nbsp;</span><span class="switch-right" ng-bind="offLabel">Off</span></div></div>',
     link: function ($scope, element, attrs) {
       attrs.$observe('onLabel', function(val) {
         $scope.onLabel = angular.isDefined(val) ? val : 'On';
@@ -23,19 +22,22 @@ angular.module('toggle-switch', ['ng']).directive('toggleSwitch', function () {
       attrs.$observe('knobLabel', function(val) {
         $scope.knobLabel = angular.isDefined(val) ? val : '\u00A0';
       });
-      $scope.$watch('enabled', function(enabled) {
-         if(!enabled && $scope.editable && $scope.model) {
+      attrs.$observe('disabled', function(disabled) {
+         if(disabled && $scope.model) {
              $scope.toggle();
          }
       });
+      $scope.$watch('enabled', function(enabled) {
+          if(!enabled && $scope.editable && $scope.model) {
+             $scope.toggle();
+          }
+      });
       $scope.toggle = function toggle() {
-          if($scope.editable) {
-              if($scope.enabled) {
-                  element.children().addClass('switch-animate');
-                  $scope.model = !$scope.model;
-              } else {
-                  $scope.model = false;
-              }
+          if(!$scope.disabled) {
+              element.children().addClass('switch-animate');
+              $scope.model = !$scope.model;
+          } else {
+              $scope.model = false;
           }
       };
     }
