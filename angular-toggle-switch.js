75744c22975b87542470c5d44df143c5ee32b53c
Martin Maestri <mmaestri@gmail.com>
2014-01-08 15:49:49 -0300

    angular-toggle-switch.js:   Text editor is trolling me big time. Set indentation on 2 spaces line by line. Hope this is it.

--- angular-toggle-switch.js
+++ angular-toggle-switch.js
@@ -1,40 +1,39 @@
 angular.module('toggle-switch', ['ng']).directive('toggleSwitch', function () {
-    return {
-        restrict: 'EA',
-        replace: true,
-        scope: {
-            model: '=',
-            disabled: '@',
-            onLabel: '@',
-            offLabel: '@',
-            knobLabel: '@'
-        },
+  return {
+    restrict: 'EA',
+    replace: true,
+    scope: {
+      model: '=',
+      disabled: '@',
+      onLabel: '@',
+      offLabel: '@',
+      knobLabel: '@'
+    },
 
-        template: '<div class="switch" ng-click="toggle()" ng-class="{ \'disabled\': disabled }"><div ng-class="{\'switch-off\': !model, \'switch-on\': model }"><span class="switch-left" ng-bind="onLabel">On</span><span class="knob" ng-bind="knobLabel">&nbsp;</span><span class="switch-right" ng-bind="offLabel">Off</span></div></div>',
-        link: function ($scope, element, attrs) {
-            attrs.$observe('onLabel', function(val) {
-                $scope.onLabel = angular.isDefined(val) ? val : 'On';
-            });
+    template: '<div class="switch" ng-click="toggle()"><div ng-class="{\'switch-off\': !model, \'switch-on\': model}"><span class="switch-left" ng-bind-html-unsafe="onLabel">On</span><span class="knob" ng-bind="knobLabel">&nbsp;</span><span class="switch-right" ng-bind-html-unsafe="offLabel">Off</span></div></div>',
+    link: function ($scope, element, attrs) {
+      attrs.$observe('onLabel', function(val) {
+        $scope.onLabel = angular.isDefined(val) ? val : 'On';
+      });
 
-            attrs.$observe('offLabel', function(val) {
-                $scope.offLabel = angular.isDefined(val) ? val : 'Off';
-            });
+      attrs.$observe('offLabel', function(val) {
+        $scope.offLabel = angular.isDefined(val) ? val : 'Off';
+      });
 
-            attrs.$observe('knobLabel', function(val) {
-                $scope.knobLabel = angular.isDefined(val) ? val : '\u00A0';
-            });
+      attrs.$observe('knobLabel', function(val) {
+        $scope.knobLabel = angular.isDefined(val) ? val : '\u00A0';
+      });
 
-            attrs.$observe('disabled', function(disabled) {
-                $scope.disabled = disabled === 'true';
-            });
+      attrs.$observe('disabled', function(disabled) {
+        $scope.disabled = disabled === 'true';
+      });
 
-            $scope.toggle = function toggle() {
-                if(!$scope.disabled) {
-                    element.children().addClass('switch-animate');
-                    $scope.model = !$scope.model;
-                }
-            };
+      $scope.toggle = function toggle() {
+        if(!$scope.disabled) {
+          element.children().addClass('switch-animate');
+          $scope.model = !$scope.model;
         }
-    };
+      };
+    }
+  };
 });
-
