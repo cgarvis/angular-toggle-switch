c7897f0ac4e4ca1bc75b6131ca764deeeb19d86b
Christopher Garvis <cgarvis@gmail.com>
2014-05-13 08:17:59 -0400

    Use optional attributes instead of compile hack

--- angular-toggle-switch.js
+++ angular-toggle-switch.js
@@ -4,24 +4,23 @@ angular.module('toggle-switch', ['ng']).directive('toggleSwitch', function () {
     replace: true,
     scope: {
       model: '=',
-      disabled: '@',
-      onLabel: '@',
-      offLabel: '@',
-      knobLabel: '@'
+      disabled: '@?',
+      onLabel: '@?',
+      offLabel: '@?',
+      knobLabel: '@?'
     },
     template: '<div class="switch" ng-click="toggle()" ng-class="{ \'disabled\': disabled }"><div class="switch-animate" ng-class="{\'switch-off\': !model, \'switch-on\': model}"><span class="switch-left" ng-bind="onLabel"></span><span class="knob" ng-bind="knobLabel"></span><span class="switch-right" ng-bind="offLabel"></span></div></div>',
     controller: function($scope) {
+      $scope.onLabel = $scope.onLabel || 'On';
+      $scope.offLabel = $scope.offLabel || 'Off';
+      $scope.knobLabel = $scope.knobLabel || '\u00a0';
+      $scope.disabled = $scope.disabled || false;
+
       $scope.toggle = function toggle() {
         if(!$scope.disabled) {
           $scope.model = !$scope.model;
         }
       };
-    },
-    compile: function(element, attrs) {
-      if (!attrs.onLabel) { attrs.onLabel = 'On'; }
-      if (!attrs.offLabel) { attrs.offLabel = 'Off'; }
-      if (!attrs.knobLabel) { attrs.knobLabel = '\u00a0'; }
-      if (!attrs.disabled) { attrs.disabled = false; }
     },
   };
 });
