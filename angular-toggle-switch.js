5c7eb1242c3a0843ff2469c99e25906c7a3b6530
Kalarrs Topham <k-dawg-klark@hotmail.com>
2014-06-24 05:01:36 -0700

    * Updated to support multiple instances, added pre link function * Removed the default class of "bootstrap-switch-large", now just place class on <toggle-switch class="bootstrap-switch-large" ng-model="myModel"></toggle-switch> * Using the ngModel Controller instead of directly editing the scope.ngModel ** now supports pristine/dirty ** works with ng-change ** currently only supports true / false (boolean)

--- angular-toggle-switch.js
+++ angular-toggle-switch.js
@@ -12,42 +12,50 @@ angular.module('toggle-switch', ['ng']).directive('toggleSwitch', function () {
 			offClass: '@',
 			ngModel: '='
 		},
-		template: '<div class="bootstrap-switch bootstrap-switch-wrapper bootstrap-switch-large bootstrap-switch-animate" ng-click="toggle()" ng-class="{\'bootstrap-switch-off\': !ngModel, \'bootstrap-switch-on\': ngModel, \'disabled\': disabled }">' +
+		template: '<div class="bootstrap-switch bootstrap-switch-wrapper bootstrap-switch-animate" ng-click="toggle()" ng-class="{\'bootstrap-switch-off\': !ngModel, \'bootstrap-switch-on\': ngModel, \'disabled\': disabled }">' +
 		'<div class="bootstrap-switch-container" >' +
 		'<span class="bootstrap-switch-handle-on" ng-class="onClass" ng-bind="onLabel"></span>' +
 		'<span class="bootstrap-switch-label" ng-bind="knobLabel"></span>' +
 		'<span class="bootstrap-switch-handle-off" ng-class="offClass" ng-bind="offLabel"></span>' +
 		'</div>' +
 		'</div>',
-		compile: function (element, attrs) {
-			if (!attrs.onLabel) {
-				attrs.onLabel = 'On';
-			}
-			if (!attrs.offLabel) {
-				attrs.offLabel = 'Off';
-			}
-			if (!attrs.onClass) {
-				attrs.onClass = 'bootstrap-switch-primary';
-			}
-			if (!attrs.offClass) {
-				attrs.offClass = 'bootstrap-switch-default';
-			}
-			if (!attrs.knobLabel) {
-				attrs.knobLabel = '';
-			}
-			if (!attrs.disabled) {
-				attrs.disabled = false;
-			}
-			return function postLink(scope) {
-				if (typeof scope.ngModel === 'undefined') {
-					scope.ngModel = false;
-				}
-				scope.toggle = function () {
-					if (!scope.disabled) {
-						scope.ngModel = !scope.ngModel;
+		compile: function () {
+			return {
+				pre: function preLink(scope, iElement, iAttrs){
+					if (!iAttrs.onLabel) {
+						iAttrs.onLabel = 'On';
 					}
-				};
-			};
+					if (!iAttrs.offLabel) {
+						iAttrs.offLabel = 'Off';
+					}
+					if (!iAttrs.onClass) {
+						iAttrs.onClass = 'bootstrap-switch-primary';
+					}
+					if (!iAttrs.offClass) {
+						iAttrs.offClass = 'bootstrap-switch-default';
+					}
+					if (!iAttrs.knobLabel) {
+						iAttrs.knobLabel = '';
+					}
+					if (!iAttrs.disabled) {
+						iAttrs.disabled = false;
+					}
+				},
+				post: function postLink(scope, element, attrs, ngModelCtrl) {
+					if(typeof ngModelCtrl.$modelValue !== 'boolean'){
+						ngModelCtrl.$setViewValue(false);
+						ngModelCtrl.$setPristine();
+						ngModelCtrl.$render();
+					}
+
+					scope.toggle = function () {
+						if (!scope.disabled) {
+							ngModelCtrl.$setViewValue(!ngModelCtrl.$modelValue);
+							ngModelCtrl.$render();
+						}
+					};
+				}
+			}
 		}
 	};
 });
