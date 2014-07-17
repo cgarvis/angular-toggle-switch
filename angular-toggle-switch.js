155d58b2f6cacfecbb9a483f442818333496e619
Kalarrs Topham <k-dawg-klark@hotmail.com>
2014-07-16 22:40:35 -0700

    * refactoring to use the angular ui bootstrap pattern * refactored to toggle switch to better fit the bootstrap convention * only supporting bootstrap 3 classes at the moment * removed input from less * updated unit test, so they could fail against the refactor (face palm) * renamed module to gc.bootstrap, will be placed in a new repo as this is a total rewrite

--- angular-toggle-switch.js
+++ angular-toggle-switch.js
@@ -1,61 +1,115 @@
-angular.module('toggle-switch', ['ng']).directive('toggleSwitch', function () {
-	return {
-		restrict: 'EA',
-		replace: true,
-		require: 'ngModel',
-		scope: {
-			disabled: '@',
-			onLabel: '@',
-			offLabel: '@',
-			knobLabel: '@',
-			onClass: '@',
-			offClass: '@',
-			ngModel: '='
-		},
-		template: '<div class="bootstrap-switch bootstrap-switch-wrapper bootstrap-switch-animate" ng-click="toggle()" ng-class="{\'bootstrap-switch-off\': !ngModel, \'bootstrap-switch-on\': ngModel, \'disabled\': disabled }">' +
-		'<div class="bootstrap-switch-container" >' +
-		'<span class="bootstrap-switch-handle-on" ng-class="onClass" ng-bind="onLabel"></span>' +
-		'<span class="bootstrap-switch-label" ng-bind="knobLabel"></span>' +
-		'<span class="bootstrap-switch-handle-off" ng-class="offClass" ng-bind="offLabel"></span>' +
-		'</div>' +
-		'</div>',
-		compile: function () {
-			return {
-				pre: function preLink(scope, iElement, iAttrs) {
-					if (!iAttrs.onLabel) {
-						iAttrs.onLabel = 'On';
-					}
-					if (!iAttrs.offLabel) {
-						iAttrs.offLabel = 'Off';
-					}
-					if (!iAttrs.onClass) {
-						iAttrs.onClass = 'bootstrap-switch-primary';
-					}
-					if (!iAttrs.offClass) {
-						iAttrs.offClass = 'bootstrap-switch-default';
-					}
-					if (!iAttrs.knobLabel) {
-						iAttrs.knobLabel = '';
-					}
-					if (!iAttrs.disabled) {
-						iAttrs.disabled = false;
-					}
-				},
-				post: function postLink(scope, element, attrs, ngModelCtrl) {
-					if (typeof ngModelCtrl.$modelValue !== 'boolean') {
-						ngModelCtrl.$setViewValue((typeof scope.ngModel === 'boolean') ? scope.ngModel : false);
-						ngModelCtrl.$setPristine();
-						ngModelCtrl.$render();
-					}
+angular.module('gc.bootstrap', ['template/toggle-switch/toggle-switch.html', 'gc.bootstrap.toggle-switch', 'ui.bootstrap.bindHtml']);
 
-					scope.toggle = function () {
-						if (!scope.disabled) {
-							ngModelCtrl.$setViewValue(!ngModelCtrl.$modelValue);
-							ngModelCtrl.$render();
+angular.module('gc.bootstrap.toggle-switch', [])
+	.constant('toggleSwitchConfig', {
+		switchClass: 'toggle-switch-animate',
+		switchOnClass: 'toggle-switch-on',
+		switchOffClass: 'toggle-switch-off',
+		switchIndeterminateClass: 'toggle-switch-indeterminate',
+		toggleEvent: 'click',
+		onClass: 'toggle-switch-primary',
+		offClass: 'toggle-switch-default',
+		onLabel: 'On',
+		offLabel: 'Off',
+		knobLabel: ''
+	})
+	.controller('ToggleSwitchController', function (toggleSwitchConfig, $interpolate, $attrs, $scope) {
+		var self = this;
+
+		// Configuration attributes
+		angular.forEach(['switchClass','onClass', 'offClass', 'onLabel', 'offLabel', 'knobLabel'], function (key) {
+			self[key] = angular.isDefined($attrs[key]) ? $interpolate($attrs[key])($scope.$parent) : toggleSwitchConfig[key];
+		});
+
+		this.switchIndeterminateClass = toggleSwitchConfig.switchIndeterminateClass || 'toggle-switch-on';
+		this.switchOnClass = toggleSwitchConfig.switchOnClass || 'toggle-switch-on';
+		this.switchOffClass = toggleSwitchConfig.switchOffClass || 'toggle-switch-off';
+
+		this.toggleEvent = toggleSwitchConfig.toggleEvent || 'click';
+	})
+	.directive('toggleSwitch', function () {
+		return {
+			restrict: 'E',
+			replace: true,
+			require: ['toggleSwitch', 'ngModel'],
+			controller: 'ToggleSwitchController',
+			scope: {},
+			templateUrl: 'template/toggle-switch/toggle-switch.html',
+			link: function postLink(scope, element, attrs, ctrls) {
+				var toggleSwitchCtrl = ctrls[0], ngModelCtrl = ctrls[1];
+
+				scope.switchClass = toggleSwitchCtrl.switchClass;
+				scope.onClass = toggleSwitchCtrl.onClass;
+				scope.offClass = toggleSwitchCtrl.offClass;
+				scope.onLabel = toggleSwitchCtrl.onLabel;
+				scope.offLabel = toggleSwitchCtrl.offLabel;
+				scope.knobLabel = toggleSwitchCtrl.knobLabel;
+
+				// Watchable attributes
+				angular.forEach(['switchClass','onClass','offClass','onLabel','offLabel','knobLabel'], function (key) {
+					attrs.$observe(key, function(value) {
+						if(value){
+							scope[key] = value;
 						}
-					};
+					});
+				});
+
+
+				function getTrueValue() {
+					return getCheckboxValue(attrs.toggleSwitchTrue, true);
 				}
-			};
-		}
-	};
-});
+
+				function getFalseValue() {
+					return getCheckboxValue(attrs.toggleSwitchFalse, false);
+				}
+
+				function getCheckboxValue(attributeValue, defaultValue) {
+					var val = scope.$eval(attributeValue);
+					return angular.isDefined(val) ? val : defaultValue;
+				}
+
+				//model -> UI
+				ngModelCtrl.$render = function () {
+					var trueValue =  angular.equals(ngModelCtrl.$modelValue, getTrueValue()),
+						falseValue =  angular.equals(ngModelCtrl.$modelValue, getFalseValue());
+
+					element.toggleClass(toggleSwitchCtrl.switchIndeterminateClass, !trueValue && !falseValue);
+
+					element.toggleClass(toggleSwitchCtrl.switchOnClass, trueValue);
+					element.toggleClass(toggleSwitchCtrl.switchOffClass, falseValue);
+				};
+
+				//ui->model
+				element.bind(toggleSwitchCtrl.toggleEvent, function () {
+					scope.$apply(function () {
+						ngModelCtrl.$setViewValue(element.hasClass(toggleSwitchCtrl.switchOnClass) ? getFalseValue() : getTrueValue());
+						ngModelCtrl.$render();
+					});
+				});
+			}
+		};
+	});
+
+angular.module('template/toggle-switch/toggle-switch.html', [])
+	.run(function ($templateCache) {
+		$templateCache.put('template/toggle-switch/toggle-switch.html',
+			'<div class="toggle-switch toggle-switch-wrapper" ng-class="switchClass">' +
+			'<div class="toggle-switch-container" >' +
+			'<span class="toggle-switch-handle-on" ng-class="onClass" bind-html-unsafe="onLabel"></span>' +
+			'<span class="toggle-switch-label" bind-html-unsafe="knobLabel"></span>' +
+			'<span class="toggle-switch-handle-off" ng-class="offClass" bind-html-unsafe="offLabel"></span>' +
+			'</div>' +
+			'</div>' +
+			'');
+	});
+
+angular.module('ui.bootstrap.bindHtml', [])
+
+	.directive('bindHtmlUnsafe', function () {
+		return function (scope, element, attr) {
+			element.addClass('ng-binding').data('$binding', attr.bindHtmlUnsafe);
+			scope.$watch(attr.bindHtmlUnsafe, function bindHtmlUnsafeWatchAction(value) {
+				element.html(value || '');
+			});
+		};
+	});
--- test/angular-toggle-switch.spec.js
+++ test/angular-toggle-switch.spec.js
@@ -1,133 +1,133 @@
-describe('Toggle Switch', function() {
-  var $scope, $compile;
+describe('Toggle Switch', function () {
+	var $scope, $compile;
 
-  var baseTemplate = '<toggle-switch model="switchState">\n</toggle-switch>';
-  var onLabelTemplate = '<toggle-switch model="switchState" on-label="CUSTOM-ON">\n</toggle-switch>';
-  var offLabelTemplate = '<toggle-switch model="switchState" off-label="CUSTOM-OFF">\n</toggle-switch>';
-  var knobLabelTemplate = '<toggle-switch model="switchState" knob-label="CUSTOM">\n</toggle-switch>';
-  var htmlLabelsTemplate = '<toggle-switch model="switchState" on-label="<i class=\'icon-ok icon-white\'></i>" off-label="<i class=\'icon-remove\'></i>">\n</toggle-switch>';
-  var disabledTemplate = '<toggle-switch model="switchState" disabled="isDisabled">\n</toggle-switch>';
+	var template = {
+		base: '<toggle-switch ng-model="switchState"></toggle-switch>',
+		'onLabel': '<toggle-switch ng-model="switchState" on-label="Yes"></toggle-switch>',
+		'offLabel': '<toggle-switch ng-model="switchState" off-label="No"></toggle-switch>',
+		'knobLabel': '<toggle-switch ng-model="switchState" knob-label="Click"></toggle-switch>',
+		'disabled': '<toggle-switch ng-model="switchState" disabled="isDisabled"></toggle-switch>'
+	};
 
-  // Load up just our module
-  beforeEach(module('toggle-switch'));
+	// Load up just our module
+	beforeEach(module('toggle-switch'));
 
-  beforeEach(inject(function($rootScope, _$compile_) {
-    // Get an isolated scope
-    $scope = $rootScope.$new();
-    $compile = _$compile_;
-  }));
+	beforeEach(inject(function ($rootScope, _$compile_) {
+		// Get an isolated scope
+		$scope = $rootScope.$new();
+		$compile = _$compile_;
+	}));
 
-  function compileDirective(template, scope) {
-    // Compile Directive
-    var elm = angular.element(template);
-    $compile(elm)(scope);
-    scope.$apply();
-    return elm;
-  }
+	function compileDirective(template, scope) {
+		// Compile Directive
+		var elm = angular.element(template);
+		$compile(elm)(scope);
+		scope.$apply();
+		return elm;
+	}
 
-  describe('default labels', function() {
-    var elm;
+	describe('default labels', function () {
+		var elm;
 
-    beforeEach(function() {
-      elm = compileDirective(baseTemplate, $scope);
-    });
+		beforeEach(function () {
+			elm = compileDirective(template.base, $scope);
+		});
 
-    it('onLabel', function() {
-      expect(elm.text()).toContain('On');
-    });
+		it('onLabel', function () {
+			expect(elm.text()).toContain('On');
+		});
 
-    it('offLabel', function() {
-      expect(elm.text()).toContain('Off');
-    });
-  });
+		it('offLabel', function () {
+			expect(elm.text()).toContain('Off');
+		});
+	});
 
-  describe('when state is null', function() {
-    it('changes model to true when clicked', function() {
-      var elm = compileDirective(baseTemplate, $scope);
-      elm.triggerHandler('click');
-      expect($scope.switchState).toEqual(true);
-    });
-  });
+	describe('when state is null', function () {
+		it('changes model to true when clicked', function () {
+			var elm = compileDirective(template.base, $scope);
+			elm.triggerHandler('click');
+			expect($scope.switchState).toEqual(true);
+		});
+	});
 
-  describe('when state is true', function() {
-    // Change state to true
-    beforeEach(function() {
-      $scope.$apply(function() {
-        $scope.switchState = true;
-      });
-    });
+	describe('when state is true', function () {
+		// Change state to true
+		beforeEach(function () {
+			$scope.$apply(function () {
+				$scope.switchState = true;
+			});
+		});
 
-    it('changes model to false when clicked', function() {
-      var elm = compileDirective(baseTemplate, $scope);
-      elm.triggerHandler('click');
-      expect($scope.switchState).toEqual(false);
-    });
-  });
+		it('changes model to false when clicked', function () {
+			var elm = compileDirective(template.base, $scope);
+			elm.triggerHandler('click');
+			expect($scope.switchState).toEqual(false);
+		});
+	});
 
-  describe('when state is false', function() {
-    // Change state to true
-    beforeEach(function() {
-      $scope.$apply(function() {
-        $scope.switchState = false;
-      });
-    });
+	describe('when state is false', function () {
+		// Change state to true
+		beforeEach(function () {
+			$scope.$apply(function () {
+				$scope.switchState = false;
+			});
+		});
 
-    it('changes model to true when clicked', function() {
-      var elm = compileDirective(baseTemplate, $scope);
-      elm.triggerHandler('click');
-      expect($scope.switchState).toEqual(true);
-    });
-  });
+		it('changes model to true when clicked', function () {
+			var elm = compileDirective(template.base, $scope);
+			elm.triggerHandler('click');
+			expect($scope.switchState).toEqual(true);
+		});
+	});
 
-  describe('when there is a custom `on-label`', function () {
-    // @TODO: figure out how to deal with html in Angular 1.2
-    //describe('is html', function() {
-    //  it('sets the on label', function() {
-    //    var elm = compileDirective(htmlLabelsTemplate, $scope);
-    //    expect(elm.html()).toContain('<i class="icon-ok icon-white"></i>');
-    //  });
-    //});
+	describe('when there is a custom `on-label`', function () {
+		// @TODO: figure out how to deal with html in Angular 1.2
+		//describe('is html', function() {
+		//  it('sets the on label', function() {
+		//    var elm = compileDirective(htmlLabelsTemplate, $scope);
+		//    expect(elm.html()).toContain('<i class="icon-ok icon-white"></i>');
+		//  });
+		//});
 
-    describe('is string', function() {
-      it('sets the on label', function() {
-        var elm = compileDirective(onLabelTemplate, $scope);
-        expect(elm.text()).toContain('CUSTOM-ON');
-      });
-    });
-  });
+		describe('is string', function () {
+			it('sets the on label', function () {
+				var elm = compileDirective(template.onLabel, $scope);
+				expect(elm.text()).toContain('Yes');
+			});
+		});
+	});
 
-  describe('when there is a custom `off-label`', function () {
-    // @TODO: figure out how to deal with html in Angular 1.2
-    //describe('is html', function() {
-    //  it('sets the on label', function() {
-    //    var elm = compileDirective(htmlLabelsTemplate, $scope);
-    //    expect(elm.html()).toContain('<i class="icon-remove"></i>');
-    //  });
-    //});
+	describe('when there is a custom `off-label`', function () {
+		// @TODO: figure out how to deal with html in Angular 1.2
+		//describe('is html', function() {
+		//  it('sets the on label', function() {
+		//    var elm = compileDirective(htmlLabelsTemplate, $scope);
+		//    expect(elm.html()).toContain('<i class="icon-remove"></i>');
+		//  });
+		//});
 
-    describe('is string', function() {
-      it('sets the on label', function() {
-        var elm = compileDirective(offLabelTemplate, $scope);
-        expect(elm.text()).toContain('CUSTOM-OFF');
-      });
-    });
-  });
+		describe('is string', function () {
+			it('sets the on label', function () {
+				var elm = compileDirective(template.offLabel, $scope);
+				expect(elm.text()).toContain('No');
+			});
+		});
+	});
 
-  describe('when there is a custom `knob-label`', function () {
-    it('sets the on label', function() {
-      var elm = compileDirective(knobLabelTemplate, $scope);
-      expect(elm.text()).toContain('CUSTOM');
-    });
-  });
+	describe('when there is a custom `knob-label`', function () {
+		it('sets the on label', function () {
+			var elm = compileDirective(template.knobLabel, $scope);
+			expect(elm.text()).toContain('Click');
+		});
+	});
 
-  describe('when toggle is disabled', function() {
-    it('ngModel does not change on click', function() {
-      $scope.switchState = true;
-      $scope.isDisabled = true;
-      var elm = compileDirective(disabledTemplate, $scope);
-      elm.triggerHandler('click');
-      expect($scope.switchState).toEqual(true);
-    });
-  });
-
+	describe('when toggle is disabled', function () {
+		it('ngModel does not change on click', function () {
+			$scope.switchState = true;
+			$scope.isDisabled = true;
+			var elm = compileDirective(template.disabled, $scope);
+			elm.triggerHandler('click');
+			expect($scope.switchState).toEqual(true);
+		});
+	});
 });
